8135188fc61ba31b422b5f9e18758c04b042f40e
YuktaAgnihotri <yuktaagnihotri8585@gmail.com>
2025-12-03 14:49:05 +0530

    ui

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ import { useState } from "react"
 export default function Navbar({ type, setType }: Props){
    
     return(<>
-        <h1 className="text-center text-4xl font-extrabold mb-10">US sales </h1>
+        <h1 className="text-center text-4xl font-extrabold mb-10">US sales for different channels</h1>
           <button className="m-4 p-3 bg-black text-white rounded-2xl" 
           onClick={()=>setType('bargraph')}> Bargraph</button>
           <button className="m-4 p-3 bg-black text-white rounded-2xl" 
